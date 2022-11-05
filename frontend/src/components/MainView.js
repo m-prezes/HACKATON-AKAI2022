ecd992ba061505a1319ff36ea2ede5ccc7cd93ad
Mateusz <mateusz.politycki@gmail.com>
2022-11-05 11:55:44 +0100

    Add roulette

--- frontend/src/components/MainView.js
+++ frontend/src/components/MainView.js
@@ -2,6 +2,7 @@ import { Card } from 'react-bootstrap';
 import Calendar from './Calendar';
 import ShopCard from './Card';
 import HabitsView from './HabitsView';
+import Roulette from './Rolette';
 import './MainView.css'
 
 function MainView({mainViewState, handleMainViewState}) {
@@ -20,7 +21,7 @@ function MainView({mainViewState, handleMainViewState}) {
         {mainViewState === 'shop' && (
             <>
                 <div className='roulette-container'>
-                    hej
+                    <Roulette />
                 </div>
                 <div className='shop-container'>
                     <HabitsView first="Your wardrobe" second="Shop" type="shop"/>
